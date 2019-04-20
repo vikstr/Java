be311eddc63a567cd92c7f895cb27c314a9ce3a1
Victoria Strenadko <31935971+vikstr@users.noreply.github.com>
2019-04-20 18:56:07 +0300

    Create Solution2001.java

---
+++ Solution2001.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        int c = a + b;
+        System.out.println(c);
+    }
+}
